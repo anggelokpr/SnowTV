7db3b2f25dc76750e15b6ed8d082d235b577a525
Kevin Pérez <79602365+anggelokpr@users.noreply.github.com>
2021-06-09 23:37:39 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,89 @@
+var canal = document.getElementById('canal');
+
+function mtvlatino() {
+	canal.src = "https://adictosalatele.com/TV/latinos/mtv/";
+}
+function tbs() {
+	canal.src = "https://adictosalatele.com/TV/latinos/tbs/";
+}
+function universal() {
+	canal.src = "https://adictosalatele.com/TV/latinos/universalchannel/";
+}
+function space() {
+	canal.src = "https://adictosalatele.com/TV/latinos/space/";
+}
+
+
+
+function tlc() {
+	canal.src = "https://adictosalatele.com/TV/latinos/tlc/";
+}
+function hah() {
+	canal.src = "https://adictosalatele.com/TV/latinos/discoveryhh/";
+}
+function tnt() {
+	canal.src = "https://adictosalatele.com/TV/latinos/tnt/";
+}
+function cinecanal() {
+	canal.src = "https://adictosalatele.com/TV/latinos/cinecanal/";
+}
+
+
+
+function tcn() {
+	canal.src = "https://adictosalatele.com/TV/latinos/tcm/";
+}
+function fx() {
+	canal.src = "https://adictosalatele.com/TV/latinos/fx/";
+}
+function warner() {
+	canal.src = "https://adictosalatele.com/TV/latinos/warner/";
+}
+function fox() {
+	canal.src = "https://adictosalatele.com/TV/latinos/fox/";
+}
+
+
+
+function tnt() {
+	canal.src = "https://adictosalatele.com/TV/latinos/tnt/";
+}
+function cn() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/cartoonnetwork/";
+}
+function dkids() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/discoverykids/";
+}
+function djun() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/disneyjunior/";
+}
+
+
+
+function dxd() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/disneyxd/";
+}
+function dishannel() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/disneychannel/";
+}
+function toon() {
+	canal.src = "https://adictosalatele.com/TV/infantiles/tooncast/";
+}
+function dischannel() {
+	canal.src = "https://adictosalatele.com/TV/espana/discovery-channel/";
+}
+
+
+
+function natg() {
+	canal.src = "https://adictosalatele.com/TV/latinos/nationalgeo/";
+}
+function fox() {
+	canal.src = "https://adictosalatele.com/TV/espana/fox/";
+}
+function axn() {
+	canal.src = "https://adictosalatele.com/TV/latinos/axn/";
+}
+function cocina() {
+	canal.src = "https://adictosalatele.com/TV/espana/cocina/";
+}
